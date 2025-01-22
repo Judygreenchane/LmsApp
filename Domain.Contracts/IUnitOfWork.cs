38125c6211451146d31c69b0e1e273e06af6746a
Luna <hanken95@gmail.com>
2025-01-22 17:13:51 +0100

    Document repo and service layer done

--- Domain.Contracts/IUnitOfWork.cs
+++ Domain.Contracts/IUnitOfWork.cs
@@ -1,9 +1,12 @@
-﻿namespace Domain.Contracts;
+﻿using LMS.Infrastructure.Repositories;
+
+namespace Domain.Contracts;
 
 public interface IUnitOfWork
 {
     ICourseRepository CourseRepository { get; }
     IModuleRepository ModuleRepository { get; }
     IActivityRepository ActivityRepository { get; }
+    IDocumentRepository DocumentRepository { get; }
     Task CompleteAsync();
 }
--- LMS.Infrastructure/Data/AutoMapperProfile.cs
+++ LMS.Infrastructure/Data/AutoMapperProfile.cs
@@ -1,7 +1,11 @@
 ﻿using AutoMapper;
 using Domain.Models.Entities;
 using LMS.Shared.DTOs;
+using LMS.Shared.DTOs.Activity;
+using LMS.Shared.DTOs.ActivityType;
 using LMS.Shared.DTOs.Course;
+using LMS.Shared.DTOs.Document;
+using LMS.Shared.DTOs.Module;
 
 
 namespace LMS.Infrastructure.Data;
@@ -14,5 +18,17 @@ public class AutoMapperProfile : Profile
         CreateMap<CourseDto, Course>().ReverseMap();
         CreateMap<CourseCreateDto, Course>().ReverseMap();
         CreateMap<CourseUpdateDto, Course>().ReverseMap();
+        CreateMap<ModuleDto, Module>().ReverseMap();
+        CreateMap<ModuleCreateDto, Module>().ReverseMap();
+        CreateMap<ModuleUpdateDto, Module>().ReverseMap();
+        CreateMap<ActivityDto, Activity>().ReverseMap();
+        CreateMap<ActivityCreateDto, Activity>().ReverseMap();
+        CreateMap<ActivityUpdateDto, Activity>().ReverseMap();
+        CreateMap<ActivityTypeDto, ActivityType>().ReverseMap();
+        CreateMap<ActivityTypeCreateDto, ActivityType>().ReverseMap();
+        CreateMap<ActivityTypeUpdateDto, ActivityType>().ReverseMap();
+        CreateMap<DocumentDto, Document>().ReverseMap();
+        CreateMap<DocumentCreateDto, Document>().ReverseMap();
+        CreateMap<DocumentUpdateDto, Document>().ReverseMap();
     }
   }
